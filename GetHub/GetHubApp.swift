503a023d5b6c03d2a9f539dc4907b6db98ac3dc4
Aisha25 <aishaalnozili83@gmail.com>
2023-12-11 10:40:30 +0300

    Initial Commit

---
+++ GetHub/GetHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetHubApp.swift
+//  GetHub
+//
+//  Created by Aisha Alnozili on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GetHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
